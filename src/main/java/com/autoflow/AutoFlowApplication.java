4284dd2bf585af8bfb9ddeb5b938519b71cae8d1
David Carrasco <davidack123456789@gmail.com>
2026-07-07 15:47:57 -0500

    arquitectura en capas

---
+++ src/main/java/com/autoflow/AutoFlowApplication.java
@@ -0,0 +1,14 @@
+package com.autoflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoFlowApplication {
+
+    public static void
+    main(String[] args) {
+        SpringApplication.run(AutoFlowApplication.class, args);
+    }
+
+}
